6bfe9030b32d534bc82bc993fb6d9020281535d0
madina myrzakulova <madina.japarbekovna@jmail.com>
2021-08-06 08:59:53 +0300

    set static and media urls and roots

--- todo/urls.py
+++ todo/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('test', test, name='test'),
     path('test2/', second)
 ]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-    +static(Settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+    +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT ) \
